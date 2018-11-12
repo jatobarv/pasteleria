7ca98c0a9fc0639ad60d8b6f5453c3b6ffb9dbd9
Javier Tobar <ja.tobarv@alumnos.duoc.cl>
2018-11-12 09:40:25 -0300

    Carga archivos

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById('simpleModal');
+
+window.addEventListener('click', clickOutside);
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+function clickOutside(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
